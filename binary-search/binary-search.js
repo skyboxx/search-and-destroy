d74104cce8baf888f2c32dd00141757ad68f0895
Andres Gomez <andresgomezjr89@gmail.com>
2020-05-28 15:28:37 -0400

    binary search done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,31 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, startIndex=0, endIndex=array.length) => {
+    if (array.length <= 0)
+        return false;
+
+    // find the actual midpoint index
+    const midpointIndex = Math.floor((startIndex + endIndex) / 2);
+
+    // cache the actual value at the midpoint
+    const midPointValue = array[midpointIndex];
+
+    // found the value
+    if (midPointValue === target)
+        return true;
+
+    // left with one value and we know its not teh target
+    if (endIndex - startIndex <= 1)
+        return false;
+
+    // if it's more we're going to use the left side
+    if (midPointValue > target) {
+        return binarySearch(array, target, startIndex, midpointIndex);
+    }
+    else {
+        return binarySearch(array, target, midpointIndex, endIndex);
+    }
 };
 
 /*
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -6,7 +6,6 @@ const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
 
-
 describe('Binary Search', () => {
 	it('returns true if target value is in array', () =>{
 		expect(binarySearch(test1, 10)).to.equal(true);
@@ -14,12 +13,12 @@ describe('Binary Search', () => {
 		expect(binarySearch(test2, 20)).to.equal(true);
 		expect(binarySearch(test3, 2.6)).to.equal(true);
 	});
-
 	it('returns false if target value is not in array', () => {
 		expect(binarySearch(test2, 25)).to.equal(false);
 		expect(binarySearch(test3, 4)).to.equal(false);
 	});
 
-	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
-
+    it('returns false if array is empty', () => {
+		expect(binarySearch([], 25)).to.equal(false);
+	});
 });
